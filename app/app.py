cf76d08e325882955962cc83a3b012f4d6f20225
Ridwan Efendi <ridwanefendi1903@gmail.com>
2023-04-30 21:11:01 +0700

    update

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
 '''
 	Contoh Deloyment untuk Domain Data Science (DS)
-	Orbit Future Academy - AI Mastery - KM Batch 3
+	Orbit Future Academy - AI Mastery - KM Batch 4
 	Tim Deployment
-	2022
+	2023
 '''
 
 # =[Modules dan Packages]========================
 
 from flask import Flask,render_template,request,jsonify
+from flask_ngrok import run_with_ngrok
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
@@ -72,9 +73,9 @@ if __name__ == '__main__':
 	# Load model yang telah ditraining
 	model = load('model_iris_dt.model')
 
-	# Run Flask di localhost 
-	app.run(host="localhost", port=5000, debug=True)
+	# Run Flask di Google Colab menggunakan ngrok
+	run_with_ngrok(app)
+	app.run()
 	
 	
-
 
